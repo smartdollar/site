cd00225984dbe8628b566fa1bf0b56b2e4c5c2c9
SD Dev <dsdprotocol@protonmail.com>
2021-02-18 21:12:56 -0300

    update to solve blank page issue

---
+++ precache-manifest.a15b8f5157065b0792d4b271e64a9172.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54528a68eb87a49422bb28d4e324002",
+    "url": "https://smartdollar.finance/index.html"
+  },
+  {
+    "revision": "5c1c6587f87d666f5be8",
+    "url": "https://smartdollar.finance/static/css/3.0e8e5ae0.chunk.css"
+  },
+  {
+    "revision": "95ffd921e0f8400c061c",
+    "url": "https://smartdollar.finance/static/js/0.4b2b8c25.chunk.js"
+  },
+  {
+    "revision": "95c2a6d9dc867031d9ba",
+    "url": "https://smartdollar.finance/static/js/10.6b9da2d4.chunk.js"
+  },
+  {
+    "revision": "5c1c6587f87d666f5be8",
+    "url": "https://smartdollar.finance/static/js/3.5983c3f3.chunk.js"
+  },
+  {
+    "revision": "e3cd3bf5907781de0d5251112a08d539",
+    "url": "https://smartdollar.finance/static/js/3.5983c3f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9285eeac8d8c55cf1e86",
+    "url": "https://smartdollar.finance/static/js/4.6a8f1733.chunk.js"
+  },
+  {
+    "revision": "33a04037ad981372fc37",
+    "url": "https://smartdollar.finance/static/js/5.2c8bde64.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "https://smartdollar.finance/static/js/5.2c8bde64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68f76cacd309cc64f7d2",
+    "url": "https://smartdollar.finance/static/js/6.57cd5b78.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "https://smartdollar.finance/static/js/6.57cd5b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f789ae1154b6f1ccc19",
+    "url": "https://smartdollar.finance/static/js/7.79a7706b.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "https://smartdollar.finance/static/js/7.79a7706b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "def511289275e40ea7d3",
+    "url": "https://smartdollar.finance/static/js/8.9880cff3.chunk.js"
+  },
+  {
+    "revision": "0d3f7317840ac89114e6",
+    "url": "https://smartdollar.finance/static/js/9.6db42c73.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "https://smartdollar.finance/static/js/9.6db42c73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4174b0f612ebb64bc416",
+    "url": "https://smartdollar.finance/static/js/main.8996ca65.chunk.js"
+  },
+  {
+    "revision": "30c30560f51af2feed4c",
+    "url": "https://smartdollar.finance/static/js/runtime-main.e3216478.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "https://smartdollar.finance/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "https://smartdollar.finance/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "https://smartdollar.finance/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "https://smartdollar.finance/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "https://smartdollar.finance/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "http://localhost:3000/precache-manifest.3612ff98c8b4eb634d7a3fdd45131485.js"
+  "https://smartdollar.finance/precache-manifest.a15b8f5157065b0792d4b271e64a9172.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://localhost:3000/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://smartdollar.finance/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
